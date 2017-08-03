c2ec1aa4db846fe7ba794c07332085b59c726b67
ThunderClapper <farhanth14@gmail.com>
2017-08-03 20:27:12 +0530

    updated code

--- makelist.py
+++ makelist.py
@@ -1,7 +1,6 @@
-burger= ["mcaloo", "mcchicken"]                             #burgers
-burgerext= ["cheese", "lettuce", "kethup"]                  #burger extras
-drinks= ["Coke", "pepsi", "milkshake"]                      #drinks
-drinksext= ["ice"]                                          #drinks Extras
+burger= ["Mcaloo", "Mcchicken"]                             #burgers
+burgerext= ["cheese", "lettuce", "ketchup"]                  #burger extras
+drinks= ["Coke", "pepsi", "chocolate milkshake", "strawberry milkshake"]           #drinks
 suborder,od={},{}
 tempso=[]
 
@@ -10,8 +9,7 @@ def makeorder(order):
     a = ""
     qty, flag, subtotal = 0, 0, 0
     for i,ord in enumerate(reversed(order)):                                #reverse travverse the string
-        if ord.isnumeric():                                                 #if word is a number
-            print("found number")
+        if ord.isnumeric():
             if flag==1:
                 qty = ord
                 if subtotal!=0:
@@ -24,6 +22,9 @@ def makeorder(order):
                 else:
                     if bool(suborder):
                         for k, v in suborder.items():
+                            if k==" NA ":
+                                print("You did'nt specify your order correctly!\nYOU SAID :", ' '.join(order) , "\nSkipping this Part of the order.")
+                                continue
                             a = a + " " + v + " " + k
                             print(qty, item, a)
                         suborder.clear()
@@ -34,6 +35,7 @@ def makeorder(order):
                 subqty = ord
                 for k,v in suborder.items():
                     a= a+ " "+ v +" "+ k
+                    w=k
                 suborder.clear()
                 od[a]=subqty
                 a=""
@@ -41,25 +43,45 @@ def makeorder(order):
 
         if ord.lower() in burgerext:                    #if word is an extra
             tempso.append(ord)
+            if bool(od):
+                for k,v  in od.items():
+                    del od[k]
+                    x=str(k).replace(" NA ", ord.lower())
+                    od[x]=v
 
 
 
         if ord.lower()=="with":                         #if word is with
-            for i in tempso:
-                suborder[i]="with"
-            tempso.clear()
-            flag=0
+            if bool(tempso):
+                for i in tempso:
+                    suborder[i]="with"
+                tempso.clear()
+            elif bool(od):
+                suborder[k]="with"
+            else:
+                suborder[" NA "]="without"
+            flag = 0
 
 
 
         if ord.lower() == "without" or ord.lower() == "no":             #if word is without
-            for i in tempso:
-                suborder[i] = "without"
-            tempso.clear()
+            if bool(tempso):
+                for i in tempso:
+                    suborder[i] = "without"
+                tempso.clear()
+            elif bool(od):
+                suborder[k]="with"
+            else:
+                suborder[" NA "]="without"
             flag=0
 
 
-        if ord.lower() in  burger:                                       #if word is a burger
+        if ord.lower() in  burger or ord.lower() in  drinks:            #if word is a burger or drink
+            if (ord.lower() in drinks) and (bool(tempso) or bool(od) or bool(suborder)):
+                print("cant have those extras with/without ", ord.lower(), "\nDefaulting to normal", ord.lower())
+                tempso.clear()
+                od.clear()
+                suborder.clear()
             if bool(od):
                 for k,v in od.items():
                     print(v, ord.lower(), k)
--- takeorder.py
+++ takeorder.py
@@ -1,28 +1,28 @@
 import speech_recognition as sr
 import pyttsx3
 import makelist
-def takeorderfunction():
-    print("INSTRUCTIONS: \n 1) Be clear \n 2) Mention Quantity, even for suborders \n 3) Avoid Repeating name for suborder")
-    engine= pyttsx3.init()
-    engine.setProperty("voice", "american")
-    engine.setProperty("rate", "140")
-    engine.say("hello, what would you like to eat?")
-    engine.runAndWait()
+import text2num
+print("INSTRUCTIONS: \n 1) Be clear \n 2) Mention Quantity, even for suborders \n 3) Avoid Repeating name for suborder")
+'''engine= pyttsx3.init()
+engine.setProperty("voice", "american")
+engine.setProperty("rate", "140")
+engine.say("hello, what would you like to eat?")
+engine.runAndWait()
 
-
-    recog = sr.Recognizer()
-    with sr.Microphone() as source:                             # use the default microphone as the audio source
-        order = recog.listen(source)                            # listen for the first phrase and extract it into audio data
-    try:
-        order=recog.recognize_google(order)
-        print("You said " + order)      # recognize speech using Google Speech Recognition
-        order = order.replace(" a ", " 1 ").replace(" a ", " 1 ")
-        order = order.split()
-        message=order
-        makelist.makeorder(order)
-    except sr.UnknownValueError:
-        message="Oops! Didn't catch that"
-    except sr.RequestError as e:
-        message="Sorry Service is Unavailible at the moment"
-
-    return message
+recog = sr.Recognizer()
+with sr.Microphone() as source:                             # use the default microphone as the audio source
+    order = recog.listen(source)                            # listen for the first phrase and extract it into audio data
+try:
+    order=recog.recognize_google(order)
+    print("You said " + order)      # recognize speech using Google Speech Recognition'''
+order=input()         #remove after testing
+order = order.replace(" a ", " 1 ").replace(" a ", " 1 ")
+order = order.split()
+for k,v in enumerate(order):
+    order[k]=str(text2num.text2num(v))
+print(order)          #remove after testing
+makelist.makeorder(order)
+'''except sr.UnknownValueError:
+    print("Oops! Didn't catch that")
+except sr.RequestError as e:
+    print("Sorry Service is Unavailible at the moment")'''
